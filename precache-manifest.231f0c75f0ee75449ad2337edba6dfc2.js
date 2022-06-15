207b13ef240127bc195143a2adba2cf494d6358e
Anubhav <Anubhavladha11@gmail.com>
2022-06-15 12:55:50 +0530

    Updates

---
+++ precache-manifest.231f0c75f0ee75449ad2337edba6dfc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1de3fba5e9f98d34077",
+    "url": "/textutils/static/css/main.1169485c.chunk.css"
+  },
+  {
+    "revision": "b1de3fba5e9f98d34077",
+    "url": "/textutils/static/js/main.b1de3fba.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/textutils/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "81cb09b4c12db38dd419",
+    "url": "/textutils/static/js/2.81cb09b4.chunk.js"
+  },
+  {
+    "revision": "21d73c2535ac79ac3b24",
+    "url": "/textutils/static/js/runtime~main.21d73c25.js"
+  },
+  {
+    "revision": "984c3670888b1074cc7d83b449e25f06",
+    "url": "/textutils/index.html"
+  }
+];
